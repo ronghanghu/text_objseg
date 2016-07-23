450a953e218bc738fa0f3b487d1eec672a69bed5
Ronghang Hu <ronghang.hu@gmail.com>
2016-07-22 18:59:13 -0700

    Fix out-of-memory issue during building segmentation batches

--- exp-referit/build_training_batches_seg.py
+++ exp-referit/build_training_batches_seg.py
@@ -37,7 +37,6 @@ input_W = 512; featmap_W = (input_W // 32)
 query_dict = json.load(open(query_file))
 imsize_dict = json.load(open(imsize_file))
 imcrop_list = query_dict.keys()
-imlist = list({name.split('_', 1)[0] + '.jpg' for name in query_dict})
 vocab_dict = text_processing.load_vocab_dict_from_file(vocab_file)
 
 ################################################################################
@@ -53,21 +52,8 @@ for n_imcrop in range(num_imcrop):
     # Image and mask
     imname = imcrop_name.split('_', 1)[0] + '.jpg'
     mask_name = imcrop_name + '.mat'
-    im = skimage.io.imread(image_dir + imname)
-    mask = load_gt_mask(mask_dir + mask_name).astype(np.float32)
-
-    processed_im = skimage.img_as_ubyte(im_processing.resize_and_pad(im, input_H, input_W))
-    if processed_im.ndim == 2:
-        processed_im = processed_im[:, :, np.newaxis]
-    processed_mask = im_processing.resize_and_pad(mask, input_H, input_W)
-    subsampled_mask = skimage.transform.downscale_local_mean(processed_mask, (32, 32))
-
-    labels_fine = (processed_mask > 0)
-    labels_coarse = (subsampled_mask > 0)
-
     for description in query_dict[imcrop_name]:
-        text_seq = text_processing.preprocess_sentence(description, vocab_dict, T)
-        training_samples.append((processed_im, text_seq, labels_coarse, labels_fine))
+        training_samples.append((imname, mask_name, description))
 
 # Shuffle the training instances
 np.random.seed(3)
@@ -95,7 +81,21 @@ for n_batch in range(num_batch):
     batch_begin = n_batch * N
     batch_end = (n_batch+1) * N
     for n_sample in range(batch_begin, batch_end):
-        processed_im, text_seq, labels_coarse, labels_fine = shuffled_training_samples[n_sample]
+        imname, mask_name, description = shuffled_training_samples[n_sample]
+        im = skimage.io.imread(image_dir + imname)
+        mask = load_gt_mask(mask_dir + mask_name).astype(np.float32)
+
+        processed_im = skimage.img_as_ubyte(im_processing.resize_and_pad(im, input_H, input_W))
+        if processed_im.ndim == 2:
+            processed_im = processed_im[:, :, np.newaxis]
+        processed_mask = im_processing.resize_and_pad(mask, input_H, input_W)
+        subsampled_mask = skimage.transform.downscale_local_mean(processed_mask, (32, 32))
+
+        labels_fine = (processed_mask > 0)
+        labels_coarse = (subsampled_mask > 0)
+
+        text_seq = text_processing.preprocess_sentence(description, vocab_dict, T)
+
         text_seq_batch[:, n_sample-batch_begin] = text_seq
         imcrop_batch[n_sample-batch_begin, ...] = processed_im
         label_coarse_batch[n_sample-batch_begin, ...] = labels_coarse[:, :, np.newaxis]
